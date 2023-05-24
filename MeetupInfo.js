2af11b8563c928617edca440a2aa80de457801d7
Olnight <106487261+Olnight@users.noreply.github.com>
2023-05-24 13:21:30 +0500

    Add files via upload

---
+++ MeetupInfo.js
@@ -0,0 +1,49 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  methods: {
+    formatAsLocalDate() {
+      return new Date(this.date).toLocaleDateString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+
+    formatAsIsoDate() {
+      return new Date(this.date).toISOString().split('T')[0];
+    },
+  },
+
+  template: `
+    <ul class="meetup-info">
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{ organizer }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{ place }}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time :datetime="formatAsIsoDate()">{{ formatAsLocalDate() }}</time>
+      </li>
+    </ul>`,
+});
